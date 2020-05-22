846a99272d93ff7dfd917c42a107777d76f218d3
francesca-campana <franx3rox@gmail.com>
2020-05-22 17:15:24 +0200

    done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var buttonAnnulla = document.getElementById('form-annulla');
 var formNome = document.getElementById('form-nome');
 var formKm = document.getElementById('form-km');
 var formEta = document.getElementById('form-eta');
+var formErrore = document.getElementById('errore');
 
 //Elementi ticket
 var ticketNome = document.getElementById('ticket-nome');
@@ -22,33 +23,41 @@ var formNomeValue;
 //Evento quando si clicca su 'Genera'
 buttonGenera.addEventListener('click',
   function() {
-    formKmValue = formKm.value;
-    formEtaValue = formEta.value;
 
-    // Calcolo Prezzo
-    var prezzo = formKmValue * 0.21;
-    var sconto = 'Nessuno sconto';
+      formKmValue = parseInt(formKm.value);
+      formEtaValue = formEta.value;
+      formNomeValue = formNome.value;
 
-    if (formEtaValue == 'minorenne') {
-      prezzo = prezzo - (prezzo * 20 / 100);
-      sconto = 'Sconto Young';
+      //Gestione Errore dati
+      if ( isNaN(formKmValue) || formNomeValue.length == 0 ) {
+        formErrore.className = 'visible';
+        console.log(formErrore);
+      }else {
+      // Calcolo Prezzo
+      var prezzo = formKmValue * 0.21;
+      var sconto = 'Nessuno sconto';
 
-    } else if (formEtaValue == 'over') {
-      prezzo = prezzo - (prezzo * 40 / 100);
-      sconto = 'Sconto Senior';
+      if (formEtaValue == 'minorenne') {
+        prezzo = prezzo - (prezzo * 20 / 100);
+        sconto = 'Sconto Young';
+
+      } else if (formEtaValue == 'over') {
+        prezzo = prezzo - (prezzo * 40 / 100);
+        sconto = 'Sconto Senior';
+      }
+      //Compilazione Ticket
+
+
+      ticketNome.innerHTML = formNome.value;
+      ticketCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
+      ticketOfferta.innerHTML = sconto;
+      ticketCarrozza.innerHTML = Math.floor((Math.random() * 10) + 1);
+      ticketCp.innerHTML = Math.floor((Math.random() * 1000) + 1);
+
+
+      //Mostra Ticket
+      ticketMostra.className = 'visible';
     }
-    //Compilazione Ticket
-
-    formNomeValue = formNome.value;
-    ticketNome.innerHTML = formNome.value;
-    ticketCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
-    ticketOfferta.innerHTML = sconto;
-    ticketCarrozza.innerHTML = Math.floor(Math.random() * 11);
-    ticketCp.innerHTML = Math.floor(Math.random() * 300);
-
-
-    //Mostra Ticket
-    ticketMostra.className = 'visible';
   }
 );
 
@@ -78,7 +87,11 @@ buttonAnnulla.addEventListener('click',
     //Gestione annullamento Carrozza
     ticketCarrozza.innerHTML = '';
 
+    //Gestione annullamento ticket
     ticketMostra.className = 'hidden';
+
+    //Gestione annullamento msf di errore
+    formErrore.className = 'hidden';
 
 
 
